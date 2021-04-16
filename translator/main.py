ed2f8feb8a2f81534ae2a436ddf96fc08d033b1e
David Suba <david.suba@ifne.eu>
2021-04-16 08:28:35 +0200

    Add copyright headers to yaml files
    Signed-off-by: David Suba <david.suba@ifne.eu>

--- translator/main.py
+++ translator/main.py
@@ -1,13 +1,17 @@
+#
 # Copyright 2020 The dNation Jsonnet Translator Authors. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 import os
 import _jsonnet
@@ -132,6 +136,7 @@ def evaluate_jsonnet_build_annotations(annotations):
     evaluated_args = {}
     for key, value in annotations.items():
         try:
+            print(value)
             evaluated_arg = ast.literal_eval(value)
             _jsonnet.evaluate_snippet("dummy", "{}", **{key: evaluated_arg})
             evaluated_args[key] = evaluated_arg
